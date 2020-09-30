4cebeeffeb42881045174af751ebd131ff7ea183
fanjiahui1998 <j-fan3276@st.kyoto-art.ac.jp>
2020-09-30 10:07:03 +0800

    c7

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+  
+    $(".aaa").on("click",()=>{
+       $("#heart").slideToggle();
+     })
+                 
+     $("#heart").slick({
+         infinite: true,
+         slidesToShow: 3,
+         slidesToScroll: 3
+       });
+ 
+ });
